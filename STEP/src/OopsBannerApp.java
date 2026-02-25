85c1d821e65facd37bc25b06097e9a2f28aead3e
yg8530-cpu8165 <yg8530@srmist.edu.in>
2026-02-25 15:00:28 +0530

    UC3

---
+++ STEP/src/OopsBannerApp.java
@@ -0,0 +1,12 @@
+public class OopsBannerApp{
+    public static void main(String[] args){
+        
+        System.out.println(String.join(" " ,"   ***   ","   ***   "," ******  ","   *****"));
+        System.out.println(String.join(" "," **   ** "," **   ** ","**    ** "," **      "));
+        System.out.println(String.join(" ","**     **","**     **","**    ** ","**       "));
+        System.out.println(String.join(" ","**     **","**     **","******   ","   ***   "));
+        System.out.println(String.join(" ","**     **","**     **","**       ","      ** "));
+        System.out.println(String.join(" "," **   ** "," **   ** ","**       ","**     **"));
+        System.out.println(String.join(" ","   ***   ","   ***   ","**       "," *****   "));
+    }
+}
